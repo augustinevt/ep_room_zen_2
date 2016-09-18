fe1adac310dce7f2ef53c9b8d14924034cef1950
Augustine von Trapp <Augustvontrapp@gmail.com>
2016-09-18 16:27:09 -0700

    add to seeds.rb to generate questions

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Question.create(title: 'How Clean Are You?')
+Question.create(title: 'What\'s Your Noise Level?')
+Question.create(title: 'How Late Are You Up Usually?')
+Question.create(title: 'How Do You Feel About Paying Rent On Time?')
+Question.create(title: 'How Feel About Sharing Food?')
+Question.create(title: 'How Do You Handle Disagreements?')
+Question.create(title: 'How Do You Feel About Parties At The House?')
+Question.create(title: 'What Do You Think About Having SOs Over?')
+Question.create(title: 'How Political Are You?')
+Question.create(title: 'Do You Use Recreational Drugs?')
