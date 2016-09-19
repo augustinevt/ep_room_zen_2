8163f21630646c1702825a16a8c6c1f696705857
Augustine von Trapp <Augustvontrapp@gmail.com>
2016-09-18 22:31:24 -0700

    add tests and code for making default responses for house model

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,6 +1,15 @@
 class House < ApplicationRecord
+
+  after_save :make_responses
+
   belongs_to :user
   has_many :requests
   has_many :applicants, through: :requests, source: :user
   has_many :responses, as: :respondable
+
+  def make_responses
+    Question.all.each do |q|
+      self.responses.create(value: 3, question_id: q.id)
+    end
+  end
 end
--- spec/models/house_spec.rb
+++ spec/models/house_spec.rb
@@ -2,4 +2,9 @@ require 'rails_helper'
 
 RSpec.describe House, type: :model do
 
+  it 'should make default responses' do
+    FactoryGirl.create_list(:question, 10)
+    house = FactoryGirl.create(:house)
+    expect(house.responses.count).to equal(10)
+  end
 end
