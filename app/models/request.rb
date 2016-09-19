100ad11c0e6234a8411e9e2df948355150ffd3ec
Augustine von Trapp <Augustvontrapp@gmail.com>
2016-09-18 23:03:12 -0700

    add model test and code for computing compatibility before saving requests

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,4 +1,21 @@
 class Request < ApplicationRecord
+
+  before_save :compute_compatibility
+
   belongs_to :user
   belongs_to :house
+
+  def compute_compatibility
+    house_responses = self.house.responses.sort_by(&:created_at)
+    applicant_responses = self.user.user_profile.responses.sort_by(&:created_at)
+
+    diff = house_responses.each_with_index.inject(0) do |sum, value|
+      sum + (value[0].value - applicant_responses[value[1]].value).abs
+    end
+
+    dividend = (Question.all.count * 4)
+    divisor = (Question.all.count * 4 - diff)
+
+    self.compatibility = (dividend.to_f/divisor.to_f * 100)
+  end
 end
--- spec/models/request_spec.rb
+++ spec/models/request_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Request, type: :model do
-
+  it 'should compute compatibility before saving' do
+    FactoryGirl.create_list(:question, 10)
+    user = FactoryGirl.create(:user_profile, username: 'KJS')
+    house = FactoryGirl.create(:house)
+    request = Request.create(user: user.user, house: house)
+    expect(request.compatibility).to equal(100)
+  end
 end
