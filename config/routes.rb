ac6695a1563a57b73a18657717635801b9bcd3c6
Augustine von Trapp <Augustvontrapp@gmail.com>
2016-09-21 20:20:18 -0700

    foo

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.
 
   resources :user_profiles do
+    resources :responses
     resources :images
   end
   resources :houses do
     resources :requests
+    resources :responses
   end
   resources :requests do
     resources :comments
   end
-
 end
