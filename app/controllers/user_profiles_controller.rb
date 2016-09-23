b2e04c1f0d0138b37a12300f5d004c4a04f46000
Augustine von Trapp <Augustvontrapp@gmail.com>
2016-09-23 16:48:43 -0700

    added a root route

--- app/controllers/user_profiles_controller.rb
+++ app/controllers/user_profiles_controller.rb
@@ -1,5 +1,9 @@
 class UserProfilesController < ApplicationController
 
+  def index
+    @user_profiles = UserProfile.all
+  end
+
   def new
     @user_profile = UserProfile.new
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.
 
+  root 'user_profiles#index'
+
   resources :user_profiles do
     resources :responses
     resources :images
