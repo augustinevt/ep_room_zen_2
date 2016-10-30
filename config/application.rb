5a5286ba737aad4c6c5c786eb637bc2f00acd39d
Augustine von Trapp <Augustvontrapp@gmail.com>
2016-10-30 14:22:51 -0700

    s3 configuration moved to config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,18 @@ module EpRoomZen2
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    Rails.application.config.paperclip_defaults = {
+      storage: :s3,
+      :s3_host_name => 's3-us-west-2.amazonaws.com',
+      s3_protocol: :https,
+      s3_region: ENV['AWS_REGION'],
+      s3_credentials: {
+        bucket: ENV['S3_BUCKET_NAME'],
+        access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+        secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+      }
+    }
+
   end
 end
